8c1fc1399e7613ab6c14ada476085fbf459d7e2c
ShashankKirad <shashank.k@shaligraminfotech.com>
2024-09-11 14:37:32 +0530

    fix: changing package size and environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,6 @@ export const environment = {
   appVersion: packageInfo.version,
   production: true,
   //apiUrl: 'http://5.77.39.57:50502/api/'
-  apiUrl: 'https://api.vel-used.com/api/',
+  // apiUrl: 'https://api.vel-used.com/api/',
+  apiUrl: 'http://192.168.3.24:7567/api/',
 };
